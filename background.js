fe7bc1b60d0f4c086e87c673b1e566aeb3271729
NIKHIL <krsoninikhil@gmail.com>
2015-11-07 09:20:52 +0530

    considering www subdomain of twitter

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.message === "time_out"){
-		chrome.tabs.query({url: ["https://www.facebook.com/*", "https://www.youtube.com/*", "https://www.twitter.com/*"]}, function(tabs){
+		chrome.tabs.query({url: ["https://www.facebook.com/*", "https://www.youtube.com/*", "https://twitter.com/*"]}, function(tabs){
 			for (var i = 0; i < tabs.length; i++) {
 				chrome.tabs.remove(tabs[i].id, function(){ } );
 				console.log("matched distractions: "+tabs[i].url);
